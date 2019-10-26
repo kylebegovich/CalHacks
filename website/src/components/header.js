4c10f1dd8bae357054da2213c4622a56eec6bc0c
Jesse <myhome2u@hotmail.com>
2019-10-26 13:48:24 -0700

    initial header and main components

---
+++ website/src/components/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <header className="header-header">
+        <div>This is the header.</div>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
---
+++ website/src/components/main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./main.css";
+
+function Main() {
+  return (
+    <div className="header">
+      <div>This is the main.</div>
+    </div>
+  );
+}
+
+export default Main;
